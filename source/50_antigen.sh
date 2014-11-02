e9d7b567043012169309838f7c226db4358f9497
Austin Angerson <austin@angerson.org>
2014-11-02 13:36:56 -0500

    Added antigen and ZSH theme.

---
+++ source/50_antigen.sh
@@ -0,0 +1,24 @@
+# Turn off GRML's prompt configuration so that antigen can
+# handle it later.
+prompt off
+
+# Load up antigen.
+source $DOTFILES/vendor/antigen/antigen.zsh
+
+# Enable use of oh-my-zsh! themes.
+antigen use oh-my-zsh
+
+# Git & git extras helpers.
+antigen bundle git
+
+# Cabal completions.
+antigen bundle cabal
+
+# Syntax highlighting on the prompt.
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Set the theme. Glorious two-line prompt!
+antigen theme tjkirch
+
+# Tell antigen we're done.
+antigen apply
