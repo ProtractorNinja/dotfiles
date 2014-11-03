f46a6b60061930bcbd76777a16f23fd00914cf9b
Austin Angerson <austin@angerson.org>
2014-11-02 22:38:36 -0500

    Fixed Vundle install for the last time.

--- init/30_ubuntu_vim_build.sh
+++ init/30_ubuntu_vim_build.sh
@@ -35,6 +35,5 @@ sudo update-alternatives --install /usr/bin/vi vi /usr/bin/gvim 1
 sudo update-alternatives --set vi /usr/bin/gvim
 
 e_header "Setting up Vundle."
-mkdir -p $DOTFILES/link/.vim/bundle/
-ln -s $DOTFILES/vendor/Vundle.vim $DOTFILES/link/.vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
